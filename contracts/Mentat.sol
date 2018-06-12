f6b63e5e083bcf03d2b4601c745779431e7f2482
Maxim Pochtar <maximpochtar@gmail.com>
2018-06-12 14:01:23 +0300

    MEN-32, MEN-33, MEN-34, MEN-53, MEN-55

--- contracts/Mentat.sol
+++ contracts/Mentat.sol
@@ -3,32 +3,36 @@ pragma solidity ^0.4.23;
 contract Mentat {
 
     /////
-    // State variables (ledger)
+    // Storage
     ///////////////////////////////
 
     address public owner;  // contract´s creator
-    enum SkillType { Skill, Expertise }
-    enum TaskStatus { Opened, Paid, Matched, Seen, Tokens_Paid, Rejected, Completed, Closed }
-    enum ChatMessageOwner { Agent, Buyer }
+    enum SkillType {Skill, Expertise}
+    enum TaskStatus {Opened, Paid, Matched, Seen, Tokens_Paid, Rejected, Completed, Closed}
+    enum ChatMessageOwner {Agent, Buyer}
 
     struct Skill {
         string name;
         SkillType skill;
-        address[] agents;
+        mapping(uint => address) agents;
+        uint agentsCount;
     }
+
     Skill[] public skills;
 
     struct Agent {
         string name;
         string email;
         bool isBusy;
-        AgentSkill[] agentSkills;
+        mapping(uint => AgentSkill) agentSkills;
+        uint agentSkillsCount;
         uint registrationTimestamp; // DateTime
         uint lastActionTimestamp; //DateTime
         uint tasksCompleted;
         uint tasksRejected;
         uint agentsReviews;
     }
+
     mapping(address => Agent) public agents;
 
     struct AgentSkill {
@@ -36,6 +40,7 @@ contract Mentat {
         uint experience;
         uint level;
     }
+
     AgentSkill[] public agentSkills;
 
     struct Task {
@@ -49,7 +54,8 @@ contract Mentat {
         bytes32 response;
         string description;
         TaskStatus status;
-        address[] rejectedAgents;
+        mapping(uint => address) rejectedAgents;
+        uint rejectedAgentsCount;
         address reviewAgent1;
         address reviewAgent2;
         address reviewAgent3;
@@ -71,173 +77,88 @@ contract Mentat {
     struct Application {
         string company;
     }
+
     Application[] public applications;
 
     ////
-    // EVENTS
+    // Events
     ///////////////
 
     event SUCCESS(string message);
     event FAIL(string message);
 
     ////
-    // Methods
+    // Modifiers
+    ///////////////
+
+    modifier isAgentRegistered(address _address) {
+        require(agents[_address].registrationTimestamp > 0);
+        _;
+    }
+
+    modifier isNotAgentRegistered(address _address) {
+        require(agents[_address].registrationTimestamp == 0);
+        _;
+    }
+
+    ////
+    // Public methods
     ///////////////
 
     constructor() public {
         owner = msg.sender;
     }
 
-    function agentSignIn() public {
-        emit SUCCESS("signedIn"); //TODO: remove stub
-        agents[msg.sender].isOffLine = false;
+    function agentSignIn()
+    isAgentRegistered(msg.sender)
+    public {
+        agents[msg.sender].lastActionTimestamp = now;
+        emit SUCCESS("signedIn");
     }
 
-    function agentSignUp(string name, string email) public {
-        emit SUCCESS("signedUp"); //TODO: remove stub
-        // Params: string name, string email, address ethAddress
-        // Returns: bool signUpOK
+    function agentSignOut()
+    isAgentRegistered(msg.sender)
+    public {
+        agents[msg.sender].lastActionTimestamp = 0;
+        emit SUCCESS("signedOut");
     }
-    function isAgentOnline(address agent) public view returns (bool) {
-        return true; //TODO: remove stub
-        
-        bool loggedIn = false;
-        if (agents[agent].isOffLine == false) {
-            loggedIn = true;
-        }
-        return loggedIn;
+
+    function agentSignUp(string _name, string _email)
+    isNotAgentRegistered(msg.sender)
+    public {
+        agents[msg.sender] = Agent({
+            name : _name,
+            email : _email,
+            isBusy : false,
+            agentSkillsCount : 0,
+            registrationTimestamp : now,
+            lastActionTimestamp : now,
+            tasksCompleted : 0,
+            tasksRejected : 0,
+            agentsReviews : 0
+            });
+        emit SUCCESS("signedUp");
+    }
+
+    function isAgentOnline(address agent) public view
+    isAgentRegistered(agent)
+    returns (bool) {
+        return ((now - agents[agent].lastActionTimestamp) < 1 hours);
     }
 
     function agentGetAction() public view returns (uint) {
-        return uint(1); //TODO: remove stub
+        return uint(1);
+        //TODO: remove stub
     }
 
     function agentGetTask() public view returns (string appName, string description) {
-        return ("Mentat Airlines", "I want to buy airplane. Which one should I buy?"); //TODO: remove stub
+        return ("Mentat Airlines", "I want to buy airplane. Which one should I buy?");
+        //TODO: remove stub
     }
 
     function agentGetReview() public view returns (string appName, string description) {
-        return ("iMentat", "I want to buy apple. Which one should I buy?"); //TODO: remove stub
+        return ("iMentat", "I want to buy apple. Which one should I buy?");
+        //TODO: remove stub
     }
-
-/*
-    function agentGetTask() public view returns(
-        uint taskId,
-        uint applicationID,
-        address buyer,
-        string description,
-        uint status, // TaskStatus
-        bool isForReview,
-        address reviewAgent1,
-        address reviewAgent2,
-        address reviewAgent3,
-        bool reviewResult1,
-        bool reviewResult2,
-        bool reviewResult3,
-        uint price,
-        uint createdAt //DateTime
-) {
-        //Task memory task = Task(1, msg.sender, msg.sender, 1,2,1,"test description", TaskStatus.Opened, false, msg.sender, msg.sender, msg.sender, true, true, true, 100, 200, 1526480941, 1526480941, 1526480941);
-        return (uint(1), uint(1), msg.sender, "test description", uint(TaskStatus.Opened), false, msg.sender, msg.sender, msg.sender, true, true, true, uint(100), uint(1526480941));
-
-        // Params: address ethAddress, uint taskID
-        // Returns: bool agentGotTask
-    }*/
-
-
-    function agentTurnOffLine(address _ethAddress) internal {
-        agents[_ethAddress].isOffLine = true;
-    }
-
-    function agentTurnOnLine(address _ethAddress) internal {
-        agents[_ethAddress].isOffLine = false;
-    }
-
-    function agentTurnBusy(address _ethAddress) internal {
-        agents[_ethAddress].isBusyNow = true;
-    }
-
-    function agentTurnAvailable(address _ethAddress) internal {
-        agents[_ethAddress].isBusyNow = false;
-    }
-
-    function agentRemoval(address _ethAddress) public {
-        delete agents[_ethAddress];
-    }
-
-    function agentAddSkill(address _ethAddress, uint _skill, uint _experiencePoints, uint _level) public {
-       // AgentSkill  auxAgent  = AgentSkill({skillID: _skill, experiencePoints: _experiencePoints, level: _level}); \\ TODO: To solve Memory x Storage issue
-       // uint i = agentSkills.push(auxAgent);
-    }
-
-    function agentUpdateAccount(address _ethAddress, string _name, string _email) public {
-        agents[_ethAddress].name = _name;
-        agents[_ethAddress].email = _email;
-    }
-
-    function agentUpdateSkillLevel() public {
-        // Params: address ethAddress, string skill, uint newLevel
-        // Returns:
-    }
-
-    function updateAgentExperiencePoints() public {
-        // Params: address ethAddress, string skill, uint newPoint
-        // Returns:
-    }
-
-    function addTask() public {
-        // Params: uint applicationID, uint skillID, uint skillLevel, uint skillMultiplier, string task, uint expectedTime, uint expectedPrice
-        // Returns: uint taskID
-    }
-
-    function agentAcceptTask() public {
-
-    }
-
-    function agentCompleteTask() public {
-
-    }
-
-    function agentRejectTask() public {
-
-    }
-
-    function checkTask() public {
-
-    }
-
-    function changeAgentTask() public {
-
-    }
-
-    function taskPriceCalculation() private {
-        // Params: uint taskID
-        // Returns: uint price
-    }
-
-    function chooseAgentForReviewing() public {
-
-    }
-
-    function agentStartReview() public {
-        // Params: address agent, uint taskID
-        // Returns:
-    }
-
-    function agentFinishReview() public {
-        // Params: address agent, uint taskID
-        // Returns: bool approved
-    }
-
-    function skillRemoval() public {
-        // Params: uint skiillID
-        // Returns:
-    }
-
-    function skillUpdate() public {
-        // Params: uint skiillID, string newName
-        // Returns:
-    }
-
 
 }
