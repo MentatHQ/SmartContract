44e0f08314f34b100249d83ed1008e1eb0f5c485
Maxim Pochtar <maximpochtar@gmail.com>
2018-06-27 15:37:12 +0300

    MEN-39, MEN-42, MEN-60, MEN-61, MEN-62, MEN-63

--- contracts/Mentat.sol
+++ contracts/Mentat.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.23;
 
-//import './MentatToken.sol';
+import './MentatToken.sol';
 
 contract Mentat {
 
@@ -8,6 +8,7 @@ contract Mentat {
     // Storage
     ///////////////////////////////
 
+    uint taskRejectionsLimit = 3;
     address public owner;  // contract´s creator
     address public mentatToken;
     enum SkillType {Skill, Expertise}
@@ -23,7 +24,7 @@ contract Mentat {
     enum ChatMessageOwner {Agent, Buyer}
 
     struct Skill {
-        string name;
+        bytes32 name;
         SkillType skill;
         mapping(uint => address) agents;
         uint agentsCount;
@@ -33,8 +34,8 @@ contract Mentat {
     uint public skillsCount;
 
     struct Agent {
-        string name;
-        string email;
+        bytes32 name;
+        bytes32 email;
         bool isBusy;
         mapping(uint => AgentSkill) agentSkills;
         uint agentSkillsCount;
@@ -45,6 +46,7 @@ contract Mentat {
         uint agentsReviews;
         uint currentTaskId;
         bool currentTaskType; // 1 - task, 0 - review
+        uint blockedUntilTimestamp;
     }
 
     mapping(address => Agent) public agents;
@@ -64,7 +66,7 @@ contract Mentat {
         uint skillLevelMultiplier;
         bytes32 request;
         bytes32 response;
-        string description;
+        bytes32 description;
         TaskStatus status;
         mapping(uint => address) rejectedAgents;
         uint rejectedAgentsCount;
@@ -93,7 +95,7 @@ contract Mentat {
     uint public tasksCount;
 
     struct Application {
-        string name;
+        bytes32 name;
     }
 
     mapping(uint => Application) public applications;
@@ -103,14 +105,14 @@ contract Mentat {
     // Events
     ///////////////
 
-    event SUCCESS(string message);
-    event FAIL(string message);
+    event SUCCESS(bytes32 message);
+    event FAIL(bytes32 message);
 
     ////
     // Modifiers
     ///////////////
 
-    modifier checkAgentRegistered(address _address) {
+    modifier checkAgentIsRegistered(address _address) {
         require(agents[_address].registrationTimestamp > 0);
         _;
     }
@@ -130,6 +132,17 @@ contract Mentat {
         _;
     }
 
+    modifier checkAgentIsNotBusy(address agent) {
+        require(!agentIsBusy(agent));
+        _;
+    }
+
+    modifier checkIsNotBlocked(address agent) {
+        require(agents[agent].blockedUntilTimestamp < now);
+        _;
+    }
+
+
     ////
     // Public methods
     ///////////////
@@ -145,20 +158,22 @@ contract Mentat {
     }
 
     function agentSignIn()
-    checkAgentRegistered(msg.sender)
+    checkAgentIsRegistered(msg.sender)
+    checkIsNotBlocked(msg.sender)
     public {
         agentUpdateOnline(msg.sender);
+        agents[msg.sender].tasksRejected = 0;
         emit SUCCESS("signedIn");
     }
 
     function agentSignOut()
-    checkAgentRegistered(msg.sender)
+    checkAgentIsRegistered(msg.sender)
     public {
         agents[msg.sender].lastActionTimestamp = 0;
         emit SUCCESS("signedOut");
     }
 
-    function agentSignUp(string _name, string _email)
+    function agentSignUp(bytes32 _name, bytes32 _email)
     isNotAgentRegistered(msg.sender)
     public {
         agents[msg.sender] = Agent({
@@ -172,26 +187,27 @@ contract Mentat {
             tasksRejected : 0,
             agentsReviews : 0,
             currentTaskId : 0,
-            currentTaskType : false
+            currentTaskType : false,
+            blockedUntilTimestamp : 0
             });
         emit SUCCESS("signedUp");
     }
 
     function isAgentRegistered(address agent) public view
-    checkAgentRegistered(agent)
+    checkAgentIsRegistered(agent)
     returns (bool)
     {
         return true;
     }
 
     function isAgentOnline(address agent) public view
-    checkAgentRegistered(agent)
+    checkAgentIsRegistered(agent)
     returns (bool) {
         return ((now - agents[agent].lastActionTimestamp) < 1 hours);
     }
 
-    function agentUpdateAccount(string _name, string _email)
-    checkAgentRegistered(msg.sender)
+    function agentUpdateAccount(bytes32 _name, bytes32 _email)
+    checkAgentIsRegistered(msg.sender)
     public {
         agents[msg.sender].name = _name;
         agents[msg.sender].email = _email;
@@ -199,20 +215,21 @@ contract Mentat {
         emit SUCCESS("agentAccountUpdated");
     }
 
-    function agentStartReview(uint _taskID) public 
-    checkAgentRegistered(msg.sender)
+    function agentStartReview(uint _taskID) public
+    checkAgentIsRegistered(msg.sender)
+    checkIsNotBlocked(msg.sender)
     returns (bool) {
         require(!agentIsBusy(msg.sender));
         agents[msg.sender].isBusy = true;
         agents[msg.sender].currentTaskId = _taskID;
         agents[msg.sender].currentTaskType = false;
         if (tasksBundle2[_taskID].reviewAgent1 != address(0)) {
-            if(tasksBundle2[_taskID].reviewAgent2 != address(0)) {
-                if(tasksBundle2[_taskID].reviewAgent3 != address(0)) {
+            if (tasksBundle2[_taskID].reviewAgent2 != address(0)) {
+                if (tasksBundle2[_taskID].reviewAgent3 != address(0)) {
                     return false;
                 } else {
                     tasksBundle2[_taskID].reviewAgent3 = msg.sender;
-                }   
+                }
             } else {
                 tasksBundle2[_taskID].reviewAgent2 = msg.sender;
             }
@@ -223,20 +240,21 @@ contract Mentat {
         agentUpdateOnline(msg.sender);
         emit SUCCESS("agentReviewStarted");
     }
-    
-    function agentFinishReview(uint _taskID, bool _result) public 
-    checkAgentRegistered(msg.sender)
+
+    function agentFinishReview(uint _taskID, bool _result) public
+    checkAgentIsRegistered(msg.sender)
+    checkIsNotBlocked(msg.sender)
     returns (bool) {
         agents[msg.sender].isBusy = false;
         agents[msg.sender].agentsReviews++;
         agents[msg.sender].currentTaskId = 0;
         if (tasksBundle2[_taskID].reviewAgent1 != msg.sender) {
-            if(tasksBundle2[_taskID].reviewAgent2 != msg.sender) {
-                if(tasksBundle2[_taskID].reviewAgent3 != msg.sender) {
+            if (tasksBundle2[_taskID].reviewAgent2 != msg.sender) {
+                if (tasksBundle2[_taskID].reviewAgent3 != msg.sender) {
                     return false;
                 } else {
                     tasksBundle2[_taskID].reviewResult3 = _result;
-                }   
+                }
             } else {
                 tasksBundle2[_taskID].reviewResult2 = _result;
             }
@@ -247,33 +265,35 @@ contract Mentat {
         tasksBundle1[_taskID].status = TaskStatus.Reviewed;
         agentUpdateOnline(msg.sender);
         emit SUCCESS("agentReviewFinished");
-    }    
+    }
 
-    function getTaskPrice(uint _taskID) public view  
+    function getTaskPrice(uint _taskID) public view
     returns (uint)  {
         return tasksBundle2[_taskID].price;
     }
-    
+
     function agentGetCurrentTaskType() public view
-    checkAgentRegistered(msg.sender)
+    checkAgentIsRegistered(msg.sender)
     returns (bool) {
         return agents[msg.sender].currentTaskType;
     }
-    
+
     function changeAgent(uint _taskID) public
-    checkAgentRegistered(msg.sender) {
+    checkAgentIsRegistered(msg.sender) {
         agents[msg.sender].isBusy = true;
         agents[msg.sender].lastActionTimestamp = now;
         agents[msg.sender].currentTaskId = _taskID;
-        agents[msg.sender].currentTaskType = true;   
+        agents[msg.sender].currentTaskType = true;
         tasksBundle1[_taskID].agent = msg.sender;
         tasksBundle1[_taskID].status = TaskStatus.Matched;
         tasksBundle1[_taskID].lastUpdateTimestamp;
     }
 
     function agentGetCurrentTask() public view
-    checkAgentRegistered(msg.sender)
-    returns (uint taskId, bool taskType, string applicationName, string description) {
+    checkAgentIsRegistered(msg.sender)
+    checkAgentIsNotBusy(msg.sender)
+    checkIsNotBlocked(msg.sender)
+    returns (uint taskId, bool taskType, bytes32 applicationName, bytes32 description) {
         taskType = agents[msg.sender].currentTaskType;
         taskId = agents[msg.sender].currentTaskId;
         require(taskId > 0);
@@ -285,15 +305,84 @@ contract Mentat {
 
     function acceptTask(uint taskId, address agent, uint tokensAmount)
     onlyMentatToken
+    checkAgentIsNotBusy(agent)
+    checkIsNotBlocked(agent)
     public {
         tasksBundle1[taskId].status = TaskStatus.Accepted;
         tasksBundle2[taskId].tokensAmount = tokensAmount;
+        //agents[agent].isBusy = true; // should set after the last review
         agentUpdateOnline(agent);
     }
-    
+
+    function rejectTask()
+    checkAgentIsNotBusy(msg.sender)
+    checkIsNotBlocked(msg.sender)
+    public {
+        agentUpdateOnline(msg.sender);
+        uint taskId = agents[msg.sender].currentTaskId;
+
+        agents[msg.sender].currentTaskId = 0;
+        agents[msg.sender].tasksRejected += 1;
+        tasksBundle1[taskId].rejectedAgentsCount += 1;
+
+        if (agents[msg.sender].tasksRejected > taskRejectionsLimit) {
+            agents[msg.sender].tasksRejected = 0;
+            agents[msg.sender].blockedUntilTimestamp = now + 1 hours;
+        }
+
+        //TODO call assign task to new agent
+    }
+
+    function completeTask(bytes32 response)
+    checkIsNotBlocked(msg.sender)
+    public {
+
+        agentUpdateOnline(msg.sender);
+        uint taskId = agents[msg.sender].currentTaskId;
+        require(agentIsBusy(msg.sender));
+        require(tasksBundle1[taskId].agent == msg.sender);
+
+        tasksBundle1[taskId].status = TaskStatus.Completed;
+        tasksBundle1[taskId].lastUpdateTimestamp = now;
+        tasksBundle1[taskId].response = response;
+
+        agents[msg.sender].currentTaskId = 0;
+        agents[msg.sender].isBusy = false;
+
+        // TODO: call assign review
+    }
+
+    function agentCheckTokensForWithdrawal()
+    checkAgentIsRegistered(msg.sender)
+    checkIsNotBlocked(msg.sender)
+    public view
+    returns (bool) {
+        uint taskId = agents[msg.sender].currentTaskId;
+        require(tasksBundle1[taskId].agent == msg.sender);
+        require(tasksBundle1[taskId].status == TaskStatus.Reviewed);
+        require(!tasksBundle2[taskId].withdrawn);
+        return true;
+    }
+
+    function agentWithdrawTokens()
+    checkAgentIsRegistered(msg.sender)
+    checkIsNotBlocked(msg.sender)
+    public {
+        uint taskId = agents[msg.sender].currentTaskId;
+
+        if(agentCheckTokensForWithdrawal()) {
+            MentatToken(mentatToken).transfer(msg.sender, tasksBundle2[taskId].tokensAmount);
+            tasksBundle1[taskId].lastUpdateTimestamp = now;
+            tasksBundle2[taskId].tokensWithdrawn = true;
+            emit SUCCESS("withdrawn");
+        } else emit FAIL("not eligible");
+    }
+
+
+
     function addTask(uint _applicationID, address _buyer, uint _skillID, uint _skillLevel, uint _skillMultiplier, bytes32 _request, uint _expectedPrice, uint _expectedCompleteTime) public {
         tasksCount++;
-        
+
         tasksBundle1[tasksCount].applicationID = _applicationID;
         tasksBundle1[tasksCount].agent = address(0);
         tasksBundle1[tasksCount].buyer = _buyer;
@@ -320,9 +409,9 @@ contract Mentat {
         tasksBundle2[tasksCount].withdrawn = false;
         tasksBundle2[tasksCount].tokensWithdrawn = false;
         tasksBundle2[tasksCount].expectedCompleteTime = _expectedCompleteTime;
-        tasksBundle2[tasksCount].completeTime = 0;    
+        tasksBundle2[tasksCount].completeTime = 0;
     }
-    
+
 
     ////
     // Internal methods
@@ -337,7 +426,7 @@ contract Mentat {
     internal
     returns (bool)
     {
-        return(agents[agent].isBusy);
+        return (agents[agent].isBusy);
     }
 
 }
