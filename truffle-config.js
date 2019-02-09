eb33d6ff5dc30b8bf33ce48d5f637bd7eefd2551
Samuel Corso <samcorso10@gmail.com>
2019-02-09 11:33:08 -0500

    Editing gas in truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -6,5 +6,11 @@ module.exports = {
       network_id: "*", // Match any network id
       gas: 6000000
     }
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
   }
 };
